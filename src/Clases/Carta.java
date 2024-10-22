a3f3d5c829f25949d62b4c1f14a02cd9fa3b38c2
Santiago Mussi <correagameroficial@gmail.com>
2024-10-22 08:51:09 -0300

    Método para comparar una carta con otras cartas

--- src/Clases/Carta.java
+++ src/Clases/Carta.java
@@ -1,8 +1,31 @@
 package Clases;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Carta {
     private String palo;
     private int numero;
+
+    //Mapa de jerarquía
+    private static final Map<String, Integer> jerarquia = new HashMap<>();
+
+    //Solo se ejecuta una vez, cuando se carga la clase, no depende de la instanciacion
+    static {
+        jerarquia.put("1 Espada", 14);
+        jerarquia.put("1 Basto", 13);
+        jerarquia.put("7 Espada", 12);
+        jerarquia.put("7 Oro", 11);
+        jerarquia.put("3", 10);
+        jerarquia.put("2", 9);
+        jerarquia.put("1", 8);
+        jerarquia.put("12", 7);
+        jerarquia.put("11", 6);
+        jerarquia.put("10", 5);
+        jerarquia.put("7", 4);
+        jerarquia.put("6", 3);
+        jerarquia.put("5", 2);
+        jerarquia.put("4", 1);
+    }
 
     //Método constructor
     public Carta(String palo, int numero){
@@ -10,8 +33,25 @@ public class Carta {
         this.numero = numero;
     }
 
-    //Getters y setters
+    //Método para obtener la clave de jerarquía
+    private String getClave() {
+        if (numero == 1 || numero == 7) {
+            return numero + " " + palo;
+        }
+        return String.valueOf(numero); // Solo el número para el resto de las cartas
+    }
 
+    // Método para comparar cartas
+    public boolean compararCon(Carta otraCarta) {
+        int valorActual = jerarquia.getOrDefault(this.getClave(), 0);
+        int valorOtra = jerarquia.getOrDefault(otraCarta.getClave(), 0);
+
+        return valorActual > valorOtra;
+    }
+
+
+
+    //Getters y setters
     public String getPalo() {
         return palo;
     }
@@ -26,5 +66,10 @@ public class Carta {
 
     public void setNumero(int numero) {
         this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return numero + " de " + palo;
     }
 }
