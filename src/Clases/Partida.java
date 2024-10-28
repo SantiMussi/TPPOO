c78f0f1d6563e564d8ea22e445d1ae37e93e8fa5
Santiago Mussi <66830160+SantiMussi@users.noreply.github.com>
2024-10-28 09:09:29 -0300

    Update Partida.java

--- src/Clases/Partida.java
+++ src/Clases/Partida.java
@@ -3,23 +3,24 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class Partida {
     private Mazo mazo;
-    private ArrayList<Jugador> jugadores;
+    private Jugador jugador1;
+    private Jugador jugador2;
     private int rondaActual;
 
-    public Partida(ArrayList<Jugador> jugadores){
+    public Partida(Jugador jugador1, Jugador jugador2){
         mazo = new Mazo();
         mazo.barajar();
-        this.jugadores = jugadores;
         rondaActual = 1;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
     }
 
     public void repartirCartas(){
-        for(Jugador jugador : jugadores){
             for(int i = 0; i< 3; i++){ // Hasta que no tenga 3 cartas le sigue dando
-                jugador.recibirCarta(mazo.repartirCarta());
+                jugador1.recibirCarta(mazo.repartirCarta());
+                jugador2.recibirCarta(mazo.repartirCarta());
             }
         }
-    }
 
     public void iniciarRonda(){
         System.out.println("Ronda "  + rondaActual);
