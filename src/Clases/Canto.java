8b6329f8a374f61b831e0e309aae9e23e99e0eed
Santiago Mussi <66830160+SantiMussi@users.noreply.github.com>
2024-10-28 08:56:46 -0300

    Creado enum de cantos

---
+++ src/Clases/Canto.java
@@ -0,0 +1,31 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public enum Canto {
+    ENVIDO(2, 1),
+    REAL_ENVIDO(3, 2),
+    FALTA_ENVIDO(0, 3),
+    TRUCO(2, 4),
+    RETRUCO(3, 5),
+    VALECUATRO(4, 6);
+
+    private final int puntos;
+    private final int prioridad;
+
+    Canto(int puntos, int prioridad) {
+        this.puntos = puntos;
+        this.prioridad = prioridad;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public int calcularFaltaEnvido() {
+    }
+}
