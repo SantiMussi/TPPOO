3a4bbc70538ef5bc868251e7b3fbfb88091893f5
Santiago Mussi <66830160+SantiMussi@users.noreply.github.com>
2024-10-28 09:22:04 -0300

    Update Ronda.java

--- src/Clases/Ronda.java
+++ src/Clases/Ronda.java
@@ -3,15 +3,35 @@ import java.util.Optional;
 public class Ronda {
     private Jugador jugador1;
     private Jugador jugador2;
-    private Optional<Canto> ultimoCanto;
+    private Canto ultimoCanto;
     private int puntosCantoActual;
 
     public Ronda(Jugador jugador1, Jugador jugador2){
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
-        this.ultimoCanto = Optional.empty();
+        this.ultimoCanto = null;
         this.puntosCantoActual = 0;
     }
 
-    public boolean realizarCanto(){}
+    public boolean realizarCanto(Jugador jugador, Canto canto){
+        if(ultimoCanto == null && canto.getPrioridad() <= ultimoCanto.getPrioridad()){
+            System.out.println("Canto no permitido, " + canto + " tiene menor o igual prioridad que " + ultimoCanto;
+            return false;
+        }
+        ultimoCanto = canto;
+        puntosCantoActual = canto.getPuntos();
+        jugador.cantar(canto);
+        return true;
+    }
+
+    public void responderCanto(Jugador jugador, boolean acepta){
+        if(ultimoCanto == null){
+            System.out.println("No hay canto al que responder");
+            return;
+        }
+
+        if(acepta){
+            System.out.println(jugador.);
+        }
+    }
 }
