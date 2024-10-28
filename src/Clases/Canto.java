9aa344401f237366d97de4c7fa5157da4b4115a2
Santiago Mussi <66830160+SantiMussi@users.noreply.github.com>
2024-10-28 09:05:41 -0300

    Update Canto.java

--- src/Clases/Canto.java
+++ src/Clases/Canto.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public enum Canto {
     ENVIDO(2, 1),
     REAL_ENVIDO(3, 2),
-    FALTA_ENVIDO(0, 3),
+    FALTA_ENVIDO(30, 3),
     TRUCO(2, 4),
     RETRUCO(3, 5),
     VALECUATRO(4, 6);
@@ -24,8 +24,5 @@ public enum Canto {
 
     public int getPrioridad() {
         return prioridad;
-    }
-
-    public int calcularFaltaEnvido() {
     }
 }
